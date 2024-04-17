016c830f2e57c9cfb6d7a78dd53056e466f79143
Santiago Guzman Jaimes <santi03312005@gmail.com>
2024-04-17 17:14:52 -0500

    changes in 404 file

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-function sumar() {
-    
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    
+// Permitir solo números en los campos de entrada
+document.getElementById("num1").addEventListener("input", function(event) {
+  this.value = this.value.replace(/[^0-9]/g, '');
+});
 
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("Por favor, ingrese números válidos.");
-      return;
-    }
-    
-    
-    var resultado = num1 + num2;
-    
-   
-    document.getElementById("resultado").innerText = "El resultado es: " + resultado;
-    
-    
-    document.getElementById("num1").value = "";
-    document.getElementById("num2").value = "";
+document.getElementById("num2").addEventListener("input", function(event) {
+  this.value = this.value.replace(/[^0-9]/g, '');
+});
+
+function sumar() {
+  // Obtener los valores de los inputs
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  
+  // Verificar si los valores son números
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("Por favor, ingrese números válidos.");
+    return;
   }
+  
+  // Calcular la suma
+  var resultado = num1 + num2;
+  
+  // Mostrar el resultado
+  document.getElementById("resultado").innerText = "El resultado es: " + resultado;
+  
+  // Borrar los valores de los inputs
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
+}
+
   
